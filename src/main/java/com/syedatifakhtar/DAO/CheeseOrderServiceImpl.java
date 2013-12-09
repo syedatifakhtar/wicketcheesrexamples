6f5558ad155092e008a35c18851db6fbb4261398
Atif <syedatifakhtar@gmail.com>
2013-12-10 01:12:22 +0530

    Added SpringMail stuff

--- src/main/java/com/syedatifakhtar/DAO/CheeseOrderServiceImpl.java
+++ src/main/java/com/syedatifakhtar/DAO/CheeseOrderServiceImpl.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.syedatifakhtar.model.Cheese;
@@ -12,12 +15,23 @@ import com.syedatifakhtar.model.Order;
 
 public class CheeseOrderServiceImpl implements CheeseOrderService{
 
+	@Autowired
+	private MailSender mailSender;
+	
 	private OrderDAO orderDAO;
 	private CheeseDAO cheeseDAO;
 	
 	
 	public CheeseDAO getCheeseDAO() {
 		return cheeseDAO;
+	}
+
+	public MailSender getMailSender() {
+		return mailSender;
+	}
+
+	public void setMailSender(MailSender mailSender) {
+		this.mailSender = mailSender;
 	}
 
 	public void setCheeseDAO(CheeseDAO cheeseDAO) {
@@ -44,6 +58,9 @@ public class CheeseOrderServiceImpl implements CheeseOrderService{
 	public long saveOrder(Order order,Map<Cheese,Integer> cheeseQuantityMap) {
 		Set<CheeseOrder> cheeseOrders	=	order.getCheeseOrder();
 		CheeseOrder cheeseOrder;
+		StringBuffer mailContent	=	new StringBuffer();
+		mailContent.append("Hi " + order.getPersonName() + ",\n\n\n");
+		mailContent.append("You ordered the following items:\n\n----------------------------------------------------------------------------------------------------\n\n");
 		long total = 0;
 		for(Cheese cheese: cheeseQuantityMap.keySet()) {
 			System.out.println("Savin cheese order for " + order.getPersonName() + " Cheese: " + cheese.getName() + " QUantity: " + cheeseQuantityMap.get(cheese));
@@ -53,12 +70,20 @@ public class CheeseOrderServiceImpl implements CheeseOrderService{
 			cheeseOrder.setQuantity(cheeseQuantityMap.get(cheese));
 			cheeseOrder.setItemPrice(cheese.getPrice());
 			cheeseOrders.add(cheeseOrder);
+			mailContent.append("Cheese: " + cheese.getName() + " Quantity: " + cheeseQuantityMap.get(cheese) + " Item Price:" + cheese.getPrice());
+			mailContent.append("\n\n\n\n----------------------------------------------------------------------------------------------------\n\n");
 			total	= total + cheeseQuantityMap.get(cheese) * cheese.getPrice();	
 		}
 		order.setTotal(total);
+		mailContent.append("\nTotal Bill: Rs " + total);
 		long orderid= orderDAO.saveOrder(order);
-		
 		System.out.println("OrderId--->" + orderid);
+		
+		SimpleMailMessage message	=	new SimpleMailMessage();
+		message.setTo("syedatifakhtar@gmail.com");
+		message.setSubject("You have mail!!");
+		message.setText(mailContent.toString());
+		mailSender.send(message);
 		return orderid;
 	}
 
